c6d8bd287eb6c0b9e09ade43e8f0fd2f8a997366
ebahr <erikson.bahr@gmail.com>
2018-12-05 22:38:59 -0200

    pushing a change... test CICD

--- ToDo.API/Startup.cs
+++ ToDo.API/Startup.cs
@@ -27,7 +27,7 @@ namespace ToDo.API
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello World!!");
             });
         }
     }
