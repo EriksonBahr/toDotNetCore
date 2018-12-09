4443b2d2030a965ee254b449519ba5d9da96cd7b
ebahr <erikson.bahr@gmail.com>
2018-12-09 12:15:55 -0200

    pushing all pending changes here... tbc

---
+++ ToDo.API/src/Infrastructure/EntityTypeConfiguration/TodoItemEntityTypeConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDo.API.Infrastructure.EntityTypeConfiguration
+{
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+    using model;
+
+    public class TodoItemEntityTypeConfiguration : IEntityTypeConfiguration<TodoItem>
+    {
+        public void Configure(EntityTypeBuilder<TodoItem> builder)
+        {
+            builder.HasKey(ci => ci.Id);
+        }
+    }
+}
